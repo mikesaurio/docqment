e145f622e52168642a99d5c31ed8b033590203ab
mikesaurio <mikesaurio@gmail.com>
2020-09-07 20:43:07 -0500

    add show create

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,37 @@
 class BoardsController < ApplicationController
+    before_action :set_board, only: [:show]
     def index
         @boards = Board.all
     end
+
+    def show
+    end
+
+    def new
+        @board = Board.new
+    end
+
+    def create
+        @board = Board.new(board_params)
+        respond_to do |format|
+            if @board.save
+                format.html {redirect_to @board}
+                format.json {render :show, status: :created, location: @board}
+            else
+                format.html {render :new}
+            end
+        end
+    end
+    
+
+
+    private
+    def set_board
+        @board = Board.find(params[:id])
+    end
+
+    def board_params
+        params.require(:board).permit(:title, :description)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users , controllers:{passwords: 'users/passwords', sessions: 'users/sessions'}
   root to: 'boards#index'
+  resources :boards, only: [:index, :show, :new, :create]
 end
